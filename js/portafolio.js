d282b8d69bfa377f2fd90f293b70688c4d9845ca
Neiver <neiverperalta@gmail.com>
2023-07-10 14:46:54 -0400

    agregacion de nuevos proyectos

--- js/portafolio.js
+++ js/portafolio.js
@@ -156,21 +156,21 @@ proyecto4.addEventListener('click', () =>{
     <div id="modal-bg" class="modal-proyecto-contenedor">
         <div class="modal-proyecto">
             <div class="modal-proyecto__img">
-                <img src="./images/Festival.png" alt="imagen del proyecto">
+                <img src="./images/SimplePhisycs2.png" alt="imagen del proyecto">
             </div>
 
             <div class="texto-contenedor">
-                <h2>Rock&EDM festival</h2>
+                <h2>Simple Phisycs</h2>
 
-                <p>practice page, recreating a ticket sales page for a music festival with HTML, CSS and JavaScript.</p>
+                <p>Physics website with different classes to learn easily. made using HTML and CSS and Bootstrap.</p>
 
                 <div class="links">
-                    <a href="https://github.com/RevienMaker/FestivalMusica.github.io" target="_blank">
+                    <a href="https://github.com/RevienMaker/SimplePhisycs.github.io" target="_blank">
                         <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
                             <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
                         </svg>
                     </a>
-                    <a href="https://revienmaker.github.io/FestivalMusica.github.io/" target="_blank">
+                    <a href="https://simple-phisycs.vercel.app/" target="_blank">
                         <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-laptop" viewBox="0 0 16 16">
                             <path d="M13.5 3a.5.5 0 0 1 .5.5V11H2V3.5a.5.5 0 0 1 .5-.5h11zm-11-1A1.5 1.5 0 0 0 1 3.5V12h14V3.5A1.5 1.5 0 0 0 13.5 2h-11zM0 12.5h16a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 12.5z"/>
                         </svg>
@@ -204,21 +204,21 @@ proyecto5.addEventListener('click', () =>{
     <div id="modal-bg" class="modal-proyecto-contenedor">
         <div class="modal-proyecto">
             <div class="modal-proyecto__img">
-                <img src="./images/CarritoCompras.png" alt="imagen del proyecto">
+                <img src="./images/EnglishUp2.png" alt="imagen del proyecto">
             </div>
 
             <div class="texto-contenedor">
-                <h2 translate="yes">Shopping Cart</h2>
+                <h2 translate="no">EnglishUp</h2>
 
-                <p>Practice page, programming a shopping cart system with JavaScript and Bootstrap.</p>
+                <p>English website with different classes to learn the language easily. made using HTML and CSS and Bootstrap.</p>
 
                 <div class="links">
-                    <a href="https://github.com/RevienMaker/CarritoComprasJS.github.io" target="_blank">
+                    <a href="https://github.com/RevienMaker/EnglishUp.github.io" target="_blank">
                         <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
                             <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
                         </svg>
                     </a>
-                    <a href="https://revienmaker.github.io/CarritoComprasJS.github.io/" target="_blank">
+                    <a href="https://english-up.vercel.app/" target="_blank">
                         <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-laptop" viewBox="0 0 16 16">
                             <path d="M13.5 3a.5.5 0 0 1 .5.5V11H2V3.5a.5.5 0 0 1 .5-.5h11zm-11-1A1.5 1.5 0 0 0 1 3.5V12h14V3.5A1.5 1.5 0 0 0 13.5 2h-11zM0 12.5h16a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 12.5z"/>
                         </svg>
